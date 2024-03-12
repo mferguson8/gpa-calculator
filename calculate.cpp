5b3c879cc07391bebfc2f1c013f4b8a837cd204f
mferguson8 <mferg3838@gmail.com>
2024-03-12 15:51:08 -0700

    added functions

--- calculate.cpp
+++ calculate.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <cmath>
+#include <list>
+#include <string>
 
+using namespace std;
+
+double calculateTotal(const list<string>& grades) {
+    double total = 0;
+    for(const string& grade : grades){
+        if(grade == "A+"){
+            total += 4.3;
+        }else if(grade == "A"){
+            total += 4;
+        }else if(grade == "A-"){
+            total += 3.7;
+        }else if(grade == "B+"){
+            total += 3.3;
+        }else if(grade == "B"){
+            total += 3;
+        }else if(grade == "B-"){
+            total += 2.7;
+        }else if(grade == "C+"){
+            total += 2.3;
+        }else if(grade == "C"){
+            total += 2;
+        }else if(grade == "C-"){
+            total += 1.7;
+        }else if(grade == "D+"){
+            total += 1.3;
+        }else if(grade == "D"){
+            total += 1;
+        }else{
+            total += 0;
+        }
+    }
+    return total;
+}
+
+double calcGPA(double total, int numClasses){
+    double gpa = total / numClasses;
+    return gpa;
+}
--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include "calculate.h"
+
+using namespace std;
+
+int numOfClasses;
+list<string> grade;
+
+int getNumOfClasses(){
+    cout << "Enter number of classes" << endl;
+    cin >> numOfClasses;
+    return numOfClasses;
+}
+
+void enterGrades(){
+    string input;
+    for(int i = 0; i <= numOfClasses; i++){
+        cout << "Enter grade for class " << i << ": ";
+        cin >> input;
+        grade.push_front(input);
+    }
+}
+
+int main(){
+    getNumOfClasses();
+    enterGrades();
+
+}
 
